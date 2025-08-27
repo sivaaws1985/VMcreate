7270a81af04dc2d69e64abed48a6222622a8cb80
sivaaws1985 <sivaaws1985@gmail.com>
2025-08-27 16:03:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,11 @@
-<<<<<<< HEAD
 
 resource "aws_instance" "foo" {
-  ami           = "ami-0f918f7e67a3323f0"
-=======
 provider "aws" {
     region = "ap-south-1"  
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-0f918f7e67a3323f0" # us-west-2
->>>>>>> 70bea5c4d7a4b41f0bf24dc287437533f0e14cf7
+  ami           = "ami-0f918f7e67a3323f0" #ami-0f918f7e67a3323f0
   instance_type = "t2.micro"
   tags = {
     Name = "TF-Instance"
