75e9b9609082747c101a4447c918ede32db27349
sivaaws1985 <sivaaws1985@gmail.com>
2025-08-27 20:32:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
     region = "ap-south-1"  
 }
 
-resource "aws_instance" "foo" {
+resource "aws_instance" "fooec2" {
   ami           = "ami-0f918f7e67a3323f0" #ami-0f918f7e67a3323f0
   instance_type = "t2.micro"
   tags = {
