ccb4f1cc93fc12fe6f52827d17c5d82dcfbcbaf8
sivaaws1985 <sivaaws1985@gmail.com>
2025-08-27 18:36:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,6 @@ resource "aws_instance" "foo" {
   ami           = "ami-0f918f7e67a3323f0" #ami-0f918f7e67a3323f0
   instance_type = "t2.micro"
   tags = {
-    Name = "TF-Instance"
+    Name = "HelloWorldEC2"
   }
 }
